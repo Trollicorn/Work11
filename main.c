5c2609fa4613a0bd498ea7f83603fb8f5cfa1998
Mohammed Uddin <muddin3@stuy.edu>
2018-10-31 00:01:16 -0400

    need to test ls -l because git bash failed me once again

--- main.c
+++ main.c
@@ -10,6 +10,37 @@ int main() {
   printf("Modes (Permissions): %d\n", meta.st_mode);
   printf("Time of Last Access: %s\n", ctime(&(meta.st_atime)));
 
+  char perm[9] = "---------";
+  int n = 0;
+  for (int m = 2; m > -1; m--){
+	for(int i = 2; i > -1; i--){
+	  if ( meta.st_mode & (1 << ((3 * i)+m)) ){
+//	  	printf("%d\n", m);
+	  	if( m == 2){
+	  		perm[n] = 'r';
+	  	}
+	  	else if (m == 1){
+	  		perm[n] = 'w';
+	  	}
+	  	else {
+	  		perm[n] = 'x';
+	  	}
+//	  	printf("%d:%c\n",n,perm[n] );
+	  }
+	  n+=3;
+	}
+	n = 3 - m;
+  }
+  n = 0;
+  while (perm[n]){
+  	printf("%c",perm[n]);
+  	n++;
+  }
+  printf("\n");
+
+
+
+
   char size[50];
   if(meta.st_size < 1024){
       sprintf(size, "The file is %ld bytes.", meta.st_size);
