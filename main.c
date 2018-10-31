b46d45870c6be6d33d5b9acd73d466eeb7a45d5f
Stefan Tan <stan3@stuy.edu>
2018-10-30 23:03:57 -0400

    updated main

--- main.c
+++ main.c
@@ -6,9 +6,25 @@
 int main() {
   struct stat meta;
   stat("main.c", &meta);
-  printf("%ld\n", meta.st_size);
-  printf("%d\n", meta.st_mode);
-  printf("%s\n", ctime(&(meta.st_atime)));
+  printf("File Size: %ld\n", meta.st_size);
+  printf("Modes (Permissions): %d\n", meta.st_mode);
+  printf("Time of Last Access: %s\n", ctime(&(meta.st_atime)));
+
+  char size[50];
+  if(meta.st_size < 1024){
+      sprintf(size, "The file is %ld bytes.", meta.st_size);
+      printf("%s\n", size);
+  }
+  else if(meta.st_size < 1048576){
+      sprintf(size, "The file is %f kilobytes.", meta.st_size / 1024.0);
+      printf("%s\n", size);
+  }
+  else if(meta.st_size < 1073741824){
+      sprintf(size, "The file is %f gigabytes.", meta.st_size / 1048576.0);
+      printf("%s\n", size);
+}
+
+
 
   return 0;
 }
